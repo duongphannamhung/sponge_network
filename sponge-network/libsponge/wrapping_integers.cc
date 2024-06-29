dc3ab3ff5ffc2543929244aebd8b7dee54ae3ac4
duongphannamhung <duongphannamhung@gmail.com>
2024-06-29 10:55:06 +0000

    done wrapping integers

--- sponge-network/libsponge/wrapping_integers.cc
+++ sponge-network/libsponge/wrapping_integers.cc
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32(isn.raw_value() + (n & 0x00000000ffffffff));
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -28,7 +27,31 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! runs from the local TCPSender to the remote TCPReceiver and has one ISN,
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
+static uint64_t distance(uint64_t absolute_seqno_case, uint64_t checkpoint) {
+    if (absolute_seqno_case >= checkpoint) {
+      return absolute_seqno_case - checkpoint;
+    } else {
+      return checkpoint - absolute_seqno_case;
+    }
+}
+
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset = n.raw_value() - isn.raw_value();
+    uint64_t x = (checkpoint & 0xffffffff00000000) + offset;
+    uint64_t d0 = distance(x, checkpoint);
+    uint64_t d1 = distance((x + (1ul << 32)), checkpoint);
+    uint64_t d2 = distance((x - (1ul << 32)), checkpoint);
+    if (d0 <= d1) {
+        if (d0 <= d2) {
+            return x;
+        } else {
+            return x - (1ul << 32);
+        }
+    } else {
+        if (d1 <= d2) {
+            return x + (1ul << 32);
+        } else {
+            return x - (1ul << 32);
+        }
+    }
 }
